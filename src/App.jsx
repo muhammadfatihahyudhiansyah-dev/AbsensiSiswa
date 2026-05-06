bc35bd7d5c9a6672106dde809072d3eac543b72f
Muhammad Fatihah Yudhiansyah <muhammadfatihahyudhiansyah@MabarEpep.local>
2026-05-06 08:25:30 +0700

    absensi

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center">
+      <div className="w-full max-w-100 bg-gray-50 min-h-screen relative shadow-2xl border-yellow-500"></div>
+    </div>
+  );
+};
+
+export default App;
